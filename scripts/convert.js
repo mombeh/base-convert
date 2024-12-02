55ff75eaae89061eed2b4da0ef64ce6f348c59d1
nadine <nadinemombeh@gmail.com>
2024-12-02 17:51:57 +0100

    resolve linters

--- scripts/convert.js
+++ scripts/convert.js
@@ -8,10 +8,10 @@ const convertedValue = document.getElementById('convertedValue')
 convertNum.addEventListener('click', () => {
   const num1 = parseInt(inputNum.value, 10)
   const num2 = parseInt(guessNumber.value, 10)
- // initialise varable
+  
   if (isNaN(num1)) {
-   convertedValue.textContent = 'Enter the corresponded number'
-   convertedValue.style.color = 'red'
+    convertedValue.textContent = 'Enter the corresponded number'
+    convertedValue.style.color = 'red'
   }
 
   const clickNumber = num1.toString(num2)
